ab9bb9501858711054db9e7ec292adb049a7de3a
aggabaorenz16 <43602006+aggabaorenz16@users.noreply.github.com>
2020-08-25 01:06:37 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function renz() {
+ alert("Facebook : \nRenz Abellanosa Aggabao")
+}
+function Facebook() {
+    alert("Facebook : \nRenz Abellanosa Aggabao")
+   }
+   function Youtube() {
+    alert("Youtube : \nRenzSkie TV")
+   }
+   function Reformat() {
+    alert("Facebook : \nRenz Abellanosa Aggabao")
+   }
