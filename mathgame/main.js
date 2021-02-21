530bd4f611c6d59b04c851ccf088356c66807618
aggabaorenz16 <43602006+aggabaorenz16@users.noreply.github.com>
2021-02-21 02:09:46 -0800

    Create main.js
    1

---
+++ mathgame/main.js
@@ -0,0 +1,24 @@
+let n1 = Math.floor(Math.random()*100*1);
+let n2 = Math.floor(Math.random()*100*1);
+
+document.getElementById("int").value = n1;
+document.getElementById("int1").value = n2;
+
+let sub = n1 * n2;
+
+function Enjoy() {
+    var game=document.getElementById("int2").value;
+    if(game == sub) {
+        document.getElementById("ans").innerHTML = "<p>Correct Answer Awesome</p>"
+    } else {
+        document.getElementById("ans").innerHTML = "<p>Incorrect Answer Try Again!!</p>"
+    } 
+    var game = document.getElementById("int2").value = "";
+  n1 = Math.floor(Math.random()*100*1);
+n2 = Math.floor(Math.random()*100*1);
+
+document.getElementById("int").value = n1;
+document.getElementById("int1").value = n2;
+
+sub = n1 * n2;
+}
